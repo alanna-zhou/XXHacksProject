d95aec9453cf26c4378d211ed5b97e8d2a2fd856
Alanna Zhou <alanna.zhou@gmail.com>
2017-02-18 14:34:02 -0800

    Add testing for katherine

--- NewUser.js
+++ NewUser.js
@@ -2,30 +2,45 @@
 var myUsername;
 var myCareer;
 var mySalary;
+var myGender;
 var index;
 var indexFound=false;
 
 //default users
-var bill_gates = {"username":"bill_gates","gender":"male","salary":11500000000, "details":"Co-Founder at Microsoft"}
-var marissa_mayer = {"username":"marissa_mayer","gender":"female","salary":36000000,"details":"CEO at Yahoo"}
+var bill_gates = {"username":"bill_gates","gender":"male","salary":11500000000, "details":"Co-Founder at Microsoft"};
+var marissa_mayer = {"username":"marissa_mayer","gender":"female","salary":36000000,"details":"CEO at Yahoo"};
 var mark_zuckerberg = {"username":"mark_zuckerberg","gender":"male","salary":1, "details":"CEO and Co-Founder at Facebook. Computer Programmer, Internet Entrepreneur."};
 
 //existing array of occupations and members
-var users = [bill_gates, marissa_mayer, mark_zuckerberg]
+var users = [bill_gates, marissa_mayer, mark_zuckerberg];
 
 function newUser(){
-  username = document.getElementById("username");
-  var isFemale = document.getElementById("optionFemale");
-  if (isFemale == true){
-    gender = "female";
-  }else{
-    gender="male";
-  }
-  salary = document.getElementById("salary");
-  //details = document.getElementById("");
-  var user = {"username":username,"gender":gender,"salary":salary}; //,"details":details
+  myUsername = document.getElementById("username").value;
+  mySalary = document.getElementById("salary").value;
+  // validateField(myUsername);
+  // validateField(mySalary);
+
+  // if (validateField(myUsername) == true || validateField(mySalary) == true) {
+  var user = {"username":myUsername,"gender":myGender,"salary":mySalary}; //,"details":details
   addUser(user);
+  window.alert(user.username);
+
+  // var isFemale = document.getElementById("optionFemale").value;
+  // if (isFemale == true){
+  //   myGender = "female";
+  // }else{
+  //   myGender = "male";
+  // }
+  //details = document.getElementById("");
+
 }
+
+// function validateField(field) {
+//   if (field == "") {
+//     alert("All fields must be filled out");
+//     return false;
+//   }
+// }
 
 function addUser(user){
   //find index
