51967aeb381f04b4ed567f44225063bcce6f1ae9
Katherine Miao <kattymiao75@gmail.com>
2017-02-18 13:43:24 -0800

    Add new files

--- NewUser.js
+++ NewUser.js
@@ -6,14 +6,15 @@ var index;
 var indexFound=false;
 
 //default users
-var bill_gates = {"username":"bill_gates","career":"engineer", "salary":11500000000, "details":"Co-Founder at Microsoft"}
-var mark_zuckerberg = {"username":"mark_zuckerberg","career":"engineer", "salary":1, "details":"CEO and Co-Founder at Facebook. Computer Programmer, Internet Entrepreneur."};
+var bill_gates = {"username":"bill_gates","gender":"male","salary":11500000000, "details":"Co-Founder at Microsoft"}
+var marissa_mayer = {"username":"marissa_mayer","gender":"female","salary":36000000,"details":"CEO at Yahoo"}
+var mark_zuckerberg = {"username":"mark_zuckerberg","gender":"male","salary":1, "details":"CEO and Co-Founder at Facebook. Computer Programmer, Internet Entrepreneur."};
 
 //existing array of occupations and members
 var users = [bill_gates, mark_zuckerberg]
 
-function newUser(username, career, salary, details){
-  var user = {"username":username,"career":career,"salary":salary,"details":details};
+function newUser(username, gender, salary, details){
+  var user = {"username":username,"gender":gender,"salary":salary,"details":details};
   addUser(user);
 }
 
@@ -29,6 +30,7 @@ function addUser(user){
   }
   if (indexFound==false){
     index=users.length;
+    indexFound=true;
   }
   //add user to user array
   users.splice(index,0,user);
